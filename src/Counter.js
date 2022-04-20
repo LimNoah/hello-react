ecbb632acce0ee39d113926574e536b422bffd13
NOAH <lnoah@NOAHui-MacBookPro.local>
2022-04-20 15:09:05 +0900

    docs: Chapter 3.4.1.4 Study

--- src/Counter.js
+++ src/Counter.js
@@ -25,12 +25,17 @@ class Counter extends Component {
             // //this.state를 사용하여 state에 새로운 값을 넣을 수 있다.
             // this.setState({ number: number + 1 });
 
-            this.setState((prevState) => {
-              return { number: prevState.number + 1 };
+            // this.setState((prevState) => {
+            //   return { number: prevState.number + 1 };
+            // });
+            // this.setState((prevState) => ({
+            //   number: prevState.number + 1,
+            // }));
+
+            this.setState({ number: number + 1 }, () => {
+              console.log("방금 setState가 호출되었습니다.");
+              console.log(this.state);
             });
-            this.setState((prevState) => ({
-              number: prevState.number + 1,
-            }));
           }}
         >
           + 1
