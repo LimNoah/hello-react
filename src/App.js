f0fe2069e7e1d50dc11ab5446e8ad2c5a9adf8bd
NOAH <lnoah@NOAHui-MacBookPro.local>
2022-04-20 15:28:31 +0900

    docs: <3.4.2.2> Using useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import MyComponent from "./MyComponent";
-import Counter from "./Counter";
+// import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
   // return <MyComponent>React</MyComponent>;
-  return <Counter />;
+  // return <Counter />;
+  return <Say />;
 };
 
 export default App;
