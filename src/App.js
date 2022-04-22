1ee1036c18a8553986284dfc75431f88e5609d21
NOAH <lnoah@NOAHui-MacBookPro.local>
2022-04-22 16:56:33 +0900

    docs: <4.2.1> Create Component related to EventPractice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import MyComponent from "./MyComponent";
 // import Counter from "./Counter";
-import Say from "./Say";
+// import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
   // return <MyComponent>React</MyComponent>;
   // return <Counter />;
-  return <Say />;
+  // return <Say />;
+  return <EventPractice />;
 };
 
 export default App;
