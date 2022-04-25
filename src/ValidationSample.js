3ec106004111cd669ce0c2b811a6c34eebf1e5be
h.jlim@kalmate.net <h.jlim@kalmate.net>
2022-04-25 16:15:44 +0900

    docs: <5.3> Taging ref to component

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -19,15 +19,24 @@ class ValidationSample extends Component {
             clicked: true,
             validated: this.state.password === '0000'
         })
+        this.input.focus();
+    }
+
+    handleKeyUp = e => {
+        if(e.key === 'Enter') {
+            this.handleButtonClick();
+        }
     }
 
     render() { 
         return (
             <div>
                 <input
+                    ref={(ref) => { this.input = ref }}
                     type="password"
                     value={this.state.password}
                     onChange={this.handleChange}
+                    onKeyUp={this.handleKeyUp}
                     className={this.state.clicked? (this.state.validated ? 'success' : 'failure') : ''}
                 />
                 <button onClick={this.handleButtonClick}>검증하기</button>
