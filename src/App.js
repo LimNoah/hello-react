34f42cfae28c1fe13db535868a2a864ccf5aefa3
h.jlim@kalmate.net <h.jlim@kalmate.net>
2022-04-25 11:11:00 +0900

    docs: <4.3> Make functional component with useState.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 // import MyComponent from "./MyComponent";
 // import Counter from "./Counter";
 // import Say from "./Say";
-import EventPractice from "./EventPractice";
+// import EventPractice from "./EventPractice";
+import EventPracticeFunctionComponent from "./EventPracticeFunctionComponent"
 
 const App = () => {
   // return <MyComponent>React</MyComponent>;
   // return <Counter />;
   // return <Say />;
-  return <EventPractice />;
+  // return <EventPractice />;
+  return <EventPracticeFunctionComponent />
 };
 
 export default App;
